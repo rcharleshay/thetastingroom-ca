7a4ae0efddfc58ea6b351b83e2225296cb5bf74a
Charlie Hay <charliehay@Charlies-MacBook.local>
2019-05-31 18:07:52 -0400

    Updates

---
+++ precache-manifest.c19d5298a80b57d3e3b330c1201b73f3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9ee05d579e62bcba274e84412ec0270",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84e86f21cddcfe2cc29f",
+    "url": "/static/js/2.19c8fbab.chunk.js"
+  },
+  {
+    "revision": "5f014472c923637dcefc",
+    "url": "/static/js/main.387143f5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13a03e3f0067084af1917fb70911ee91",
+    "url": "/static/media/banner-image.13a03e3f.jpg"
+  },
+  {
+    "revision": "ee56e6fb8331ee7376b3495d4adaf348",
+    "url": "/static/media/cocogoose.ee56e6fb.ttf"
+  },
+  {
+    "revision": "4e7a17d0291fcde780b1fb843438dba4",
+    "url": "/static/media/food-line-1.4e7a17d0.jpg"
+  },
+  {
+    "revision": "4d4068c34cbbbd3ba0ebd66b20d7067c",
+    "url": "/static/media/food-line-2.4d4068c3.jpg"
+  },
+  {
+    "revision": "4c8a1a27766adf27ed7a632405ab605f",
+    "url": "/static/media/food-line-3.4c8a1a27.jpg"
+  },
+  {
+    "revision": "7c19801e1196287c2c2dd64eae2792e4",
+    "url": "/static/media/food-line-4.7c19801e.jpg"
+  },
+  {
+    "revision": "57eee26edbfb37a44818eab379b31a2a",
+    "url": "/static/media/maps.57eee26e.jpg"
+  },
+  {
+    "revision": "c28ee7030e3a0c341a3ca7771d82cd9e",
+    "url": "/static/media/tasting-room-logo.c28ee703.png"
+  },
+  {
+    "revision": "89ba38371e27e8e8f5784113bdd2431b",
+    "url": "/static/media/wine-flight.89ba3837.jpg"
+  },
+  {
+    "revision": "29fb8873cd37aaba79ecd5f299832a06",
+    "url": "/static/media/wine-red.29fb8873.jpg"
+  },
+  {
+    "revision": "2a0bf921ecaa28081cc0ffa1e4506eb0",
+    "url": "/static/media/wine-white.2a0bf921.jpg"
+  }
+]);
