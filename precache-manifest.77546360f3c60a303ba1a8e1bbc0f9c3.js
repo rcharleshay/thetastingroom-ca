4daa207a817bb91b426ab9caafd48cd5c52a7e83
Charlie Hay <charliehay@Charlies-MacBook.local>
2019-05-31 18:09:10 -0400

    Updates

---
+++ precache-manifest.77546360f3c60a303ba1a8e1bbc0f9c3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01f41c661cc231b67aa9f1323e1dc19c",
+    "url": "/thetastingroom.ca/index.html"
+  },
+  {
+    "revision": "330b8c0dae92be765b35",
+    "url": "/thetastingroom.ca/static/js/2.497ac1e8.chunk.js"
+  },
+  {
+    "revision": "5f014472c923637dcefc",
+    "url": "/thetastingroom.ca/static/js/main.387143f5.chunk.js"
+  },
+  {
+    "revision": "ce6ac65afd8446dd2315",
+    "url": "/thetastingroom.ca/static/js/runtime~main.d595df94.js"
+  },
+  {
+    "revision": "13a03e3f0067084af1917fb70911ee91",
+    "url": "/thetastingroom.ca/static/media/banner-image.13a03e3f.jpg"
+  },
+  {
+    "revision": "ee56e6fb8331ee7376b3495d4adaf348",
+    "url": "/thetastingroom.ca/static/media/cocogoose.ee56e6fb.ttf"
+  },
+  {
+    "revision": "4e7a17d0291fcde780b1fb843438dba4",
+    "url": "/thetastingroom.ca/static/media/food-line-1.4e7a17d0.jpg"
+  },
+  {
+    "revision": "4d4068c34cbbbd3ba0ebd66b20d7067c",
+    "url": "/thetastingroom.ca/static/media/food-line-2.4d4068c3.jpg"
+  },
+  {
+    "revision": "4c8a1a27766adf27ed7a632405ab605f",
+    "url": "/thetastingroom.ca/static/media/food-line-3.4c8a1a27.jpg"
+  },
+  {
+    "revision": "7c19801e1196287c2c2dd64eae2792e4",
+    "url": "/thetastingroom.ca/static/media/food-line-4.7c19801e.jpg"
+  },
+  {
+    "revision": "57eee26edbfb37a44818eab379b31a2a",
+    "url": "/thetastingroom.ca/static/media/maps.57eee26e.jpg"
+  },
+  {
+    "revision": "c28ee7030e3a0c341a3ca7771d82cd9e",
+    "url": "/thetastingroom.ca/static/media/tasting-room-logo.c28ee703.png"
+  },
+  {
+    "revision": "89ba38371e27e8e8f5784113bdd2431b",
+    "url": "/thetastingroom.ca/static/media/wine-flight.89ba3837.jpg"
+  },
+  {
+    "revision": "29fb8873cd37aaba79ecd5f299832a06",
+    "url": "/thetastingroom.ca/static/media/wine-red.29fb8873.jpg"
+  },
+  {
+    "revision": "2a0bf921ecaa28081cc0ffa1e4506eb0",
+    "url": "/thetastingroom.ca/static/media/wine-white.2a0bf921.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c19d5298a80b57d3e3b330c1201b73f3.js"
+  "/thetastingroom.ca/precache-manifest.77546360f3c60a303ba1a8e1bbc0f9c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thetastingroom.ca/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
